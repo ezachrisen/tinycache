6730e31dc05fce83862605d2ab6c27324923d078
Espen Zachrisen <Espen.Zachrisen@AlticeUSA.com>
2020-12-02 13:45:14 -0600

    Added examples

--- example_test.go
+++ example_test.go
@@ -7,10 +7,36 @@ import (
 	cache "github.com/ezachrisen/tinycache"
 )
 
-func ExampleCache() {
-
+// Example of simple usage
+func ExampleSuccess() {
 	c := cache.NewCache()
 	c.Set("donald", "duck", 1000*time.Millisecond)
+
+	if d, ok := c.Get("donald"); ok {
+		fmt.Println("donald", d)
+	}
+	// Output: donald duck
+}
+
+// Example of a cache entry expiring
+func ExampleExpired() {
+	c := cache.NewCache()
+	c.Set("donald", "duck", 1000*time.Millisecond)
+	time.Sleep(time.Duration(time.Millisecond) * 1000)
+
+	if d, ok := c.Get("donald"); ok {
+		fmt.Println("donald", d)
+	} else {
+		fmt.Println("donald expired")
+	}
+	// Output: donald expired
+}
+
+// Example of a cache entry that never expires
+func ExampleNoExpiration() {
+	c := cache.NewCache()
+	c.Set("donald", "duck", cache.NoExpiration)
+	time.Sleep(time.Duration(time.Millisecond) * 1000)
 
 	if d, ok := c.Get("donald"); ok {
 		fmt.Println("donald", d)
